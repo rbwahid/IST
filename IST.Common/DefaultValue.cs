f3a8a7b9d0327c624d58d807b4b1fe353032bd0a
S. M. Shabriar Hossain <shabriar@encodersbd.com>
2019-07-25 16:26:48 +0600

    project setup+ user Authentication

---
+++ IST.Common/DefaultValue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IST.Common
+{
+    public static class DefaultValue 
+    {
+        public static string CompanyFullName => "Encoder Infotech LTD";
+        public static string CompanyShortName => "EIL";
+        public static string DeveloperCompanyFullName => "Encoder Infotech Limited";
+        public static string DeveloperCompanyShortName => "Encoder";
+        public static string UserPassword => "12345";
+        public static string UserResetPassword => "12345";
+    }
+}
---
+++ IST.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IST.MvcApplication" Language="C#" %>
