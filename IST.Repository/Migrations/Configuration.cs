5e9ee51bb8075bf281f59063799409b9e6a69860
S. M. Shabriar Hossain <shabriar@encodersbd.com>
2019-07-25 18:27:34 +0600

    error solve

--- IST.Repository/Migrations/Configuration.cs
+++ IST.Repository/Migrations/Configuration.cs
@@ -1,9 +1,13 @@
+
+using IST.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
 namespace IST.Repository.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
+
 
     internal sealed class Configuration : DbMigrationsConfiguration<IST.Repository.ISTDbContext>
     {
@@ -18,6 +22,31 @@ namespace IST.Repository.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+            var seedRoles = new List<UserRole>
+            {
+                new UserRole {RoleName = "Administrator", Status = 2},
+                new UserRole {RoleName = "Admin", Status = 1},
+                new UserRole {RoleName = "Manager", Status = 1},
+            };
+            seedRoles.ForEach(s => context.UserRoles.AddOrUpdate(r => r.RoleName, s));
+            context.SaveChanges();
+            var seedRolePermissionList = new List<UserRolePermission>
+            {
+                new UserRolePermission{RoleId = context.UserRoles.FirstOrDefault(x => x.RoleName == "Administrator").Id, Permission = "Global_SupAdmin"},
+
+            };
+            seedRolePermissionList.ForEach(s => context.UserRolePermissions.AddOrUpdate(u => u.Permission, s));
+            context.SaveChanges();
+            #region users seed
+            var users = new List<User>
+            {
+                new User { FullName="Administrator",UserName = "admin",Password = "827ccb0eea8a706c4c34a16891f84e7b",RoleId = context.UserRoles.FirstOrDefault(x => x.RoleName == "Administrator").Id,SupUser = true, Status=2,LastPassChangeDate = DateTime.Now,PasswordChangedCount=1},//12345
+                new User { FullName="Development",UserName = "dev",Password = "827ccb0eea8a706c4c34a16891f84e7b",RoleId = context.UserRoles.FirstOrDefault(x => x.RoleName == "Administrator").Id,SupUser = true, Status=2,LastPassChangeDate = DateTime.Now,PasswordChangedCount=1}, //12345
+               
+            };
+            users.ForEach(s => context.Users.AddOrUpdate(u => u.UserName, s));
+            context.SaveChanges();
+            #endregion
         }
     }
 }
